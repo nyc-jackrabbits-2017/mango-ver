a8184849ab4241e3732eec22f57aacb9124a8c73
Albert Farhi <email": null>
2016-12-15 17:36:44 -0500

    add vote buttons and functionality

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,3 +1,25 @@
 get '/votes/new' do
   erb :'votes/new'
 end
+
+post '/votes' do
+  @vote = Vote.new(params[:vote])
+  @vote.user = current_user
+  if @vote.save
+    "Thank you for your vote"
+  else
+    @errors = @vote.errors.full_messages
+    erb :'votes/new'
+  end
+end
+
+  put '/votes' do
+    @vote = Vote.find_by(user: current_user)
+    @vote.vote_value = params[:vote][:vote_value]
+    if @vote.save
+      "Thank you for your vote"
+    else
+      @errors = @vote.errors.full_messages
+      erb :'votes/new'
+    end
+  end
