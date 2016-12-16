01dce48ca0d5acaf295c85824c48b64896c37042
Natalie Liles <Natalieliles@gmail.com>
2016-12-16 12:51:16 -0500

    Edit vote button

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $("#display-answer").on("submit", function(event) {
+    event.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  })
+
 });
